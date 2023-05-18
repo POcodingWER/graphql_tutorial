891b631d69f45e5820c9ec13ab674669da9e07ee
lim.cs <cs.lim@fingerverse.co.kr>
2023-05-18 14:56:37 +0900

    get 요청시 argument 가져오는 방법

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first hello",
+  },
+  {
+    id: "2",
+    text: "second hello",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
     userName: String!
     firstName: String!
-    lastName: String!
+    lastName: String
   }
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    ping: String!
   }
   type Mutation {
-    postTweet(text: String!, userId: ID!): Tweet!
-    deleteTweet(id: ID!): Boolean! 
+    postTweet(text: String!, userId: ID!): Tweet
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) { // 2번째 인자에서 아규먼트 가져올수있음
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
